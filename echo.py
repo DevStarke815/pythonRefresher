04ebc9f5f2c0f5fa4e29f0994013192dda796845
DevStarke815 <Devonstarke497@gmail.com>
2025-02-08 17:36:06 -0600

    working fib

--- echo.py
+++ echo.py
@@ -4,7 +4,7 @@ def echo(text: str, repetitions: int = 3) -> str:
     """Imitate a real-world echo."""
     virt_echo=""
     for i in range(repetitions, 0, -1):
-        virt_echo += text[-i:] + "\n" #Takes our last i characters, gives us our fading effect and a newline
+        virt_echo += text[-i:] + "\n" #takes our last i characters, gives us our fading effect and a newline
     return virt_echo
 
 
--- fib.py
+++ fib.py
@@ -1,11 +1,25 @@
+# fib.py
 from functools import lru_cache
 import time
 
-# fib.py
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter() #init timer
+        result = func(*args, **kwargs) #execute
+        end_time = time.perf_counter() #end the timeR
+        duration = end_time - start_time 
+        print(f"Finished in {duration:.8f}s: f({args[0]}) -> {result}")
+        return result
+    return wrapper
+
 
 @lru_cache
 @timer
 def fib(n: int) -> int:
+    if n < 2:
+        return n
+    return fib(n - 1) + fib(n-2)
+
     ...
 
 if __name__ == "__main__":
