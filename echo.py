879d39184dbbffe26a145ea3b005e469a0912328
DevStarke815 <Devonstarke497@gmail.com>
2025-02-08 14:20:41 -0600

    changes to echo

--- echo.py
+++ echo.py
@@ -2,7 +2,13 @@
 
 def echo(text: str, repetitions: int = 3) -> str:
     """Imitate a real-world echo."""
-    
+    virt_echo=""
+    for i in range(repetitions, 0, -1):
+        virt_echo += text[-i:] + "\n" #Takes our last i characters, gives us our fading effect and a newline
+    return virt_echo
+
+
+
 
 if __name__ == "__main__":
     text = input("Yell something at a mountain: ")
